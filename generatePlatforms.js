7ade1fbf00bf7fa469d4c00e2c143104d21c6a5d
Bigbug314 <80037817+Bigbug314@users.noreply.github.com>
2021-03-05 16:49:15 +0100

    Delete generatePlatforms.js

--- generatePlatforms.js
+++
@@ -1,8 +0,0 @@
-function generatePlatforms() {
-    return [
-        new Platform(330, 290, 120, 25, images.platform2),
-        new Platform(500, 420, 70, 25, images.platform1),
-        new Platform(600, 350, 50, 25, images.platform1),
-        new Platform(200, 200, 100, 25, images.platform2)
-    ];
-}
