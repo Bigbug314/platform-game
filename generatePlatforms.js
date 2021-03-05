96b16e79149a0b54642a15f1e5d5e3fcdc6be9da
pokergamer <80112484+pokergamer@users.noreply.github.com>
2021-03-05 20:50:36 +0300

    Delete generatePlatforms.js

--- generatePlatforms.js
+++
@@ -1,8 +0,0 @@
-function generatePlatforms() {
-    return [
-        new Platform(380, 370, 80, 15, images.platform1),
-        new Platform(500, 450, 40, 15, images.platform1),
-        new Platform(580, 380, 30, 15, images.platform1),
-        new Platform(300, 300, 70, 15, images.platform1)
-    ];
-}
