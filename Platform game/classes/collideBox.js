d81ff34301c0a8a8d424427d25dc727202e4ab0f
Bigbug314 <80037817+Bigbug314@users.noreply.github.com>
2021-03-06 11:01:07 +0100

    Delete Platform game directory

--- Platform game/classes/collideBox.js
+++
@@ -1,7 +0,0 @@
-class CollideBox {
-  constructor(x, y, w, h) {
-    this.pos = createVector(x, y);
-    this.size = createVector(w, h);
-  }
-}
-
--- Platform game/classes/platform.js
+++
@@ -1,6 +0,0 @@
-class Platform extends Sprite {
-  constructor(x, y, w, h, img, /*collideBoxes*/) {
-    super(x, y, w, h, img);
-    this.collideBox = new CollideBox(this.pos.x, this.pos.y, this.size.x, this.size.y);
-  }
-}
