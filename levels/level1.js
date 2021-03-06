b803492c15e4d9f3748f529146d7de9620ae7658
Bigbug314 <bigbug31415@gmail.com>
2021-03-06 17:14:34 +0100

    Update level1.js

--- levels/level1.js
+++ levels/level1.js
@@ -3,15 +3,15 @@ function getDataLevel1() {
     platforms: [
       new Platform(-100, -2850, 100, 3800, images.brickwall), //left wall
       new Platform(-10000, 470, 20000, 100, images.platform1), //floor
-      new Platform(330, 290, 120, 25, images.platform2),
-      new Platform(500, 420, 70, 25, images.platform1),
-      new Platform(600, 350, 50, 25, images.platform1),
-      new Platform(200, 200, 100, 25, images.platform2),
-      new MovingPlatform(380, -20, 90, 25, images.platform2, "vertical", 170, 1.5),
-      new Platform(500, -50, 300, 25, images.platform2),
-      new BouncyPlatform(900, 100, 70, 10, images.slime, 15),
-      new Platform(1050, -200, 100, 25, images.platform2),
-      new Spike(1050, -215, 50, 15, images.spike)
+      new Platform(330, 270, 120, 25, images.platform2),
+      new Platform(500, 400, 70, 25, images.platform1),
+      new Platform(600, 330, 50, 25, images.platform1),
+      new Platform(200, 180, 100, 25, images.platform2),
+      new MovingPlatform(380, -40, 90, 25, images.platform2, "vertical", 170, 1.5),
+      new Platform(500, -70, 300, 25, images.platform2),
+      new BouncyPlatform(900, 80, 70, 10, images.slime, 15),
+      new Platform(1050, -220, 100, 25, images.platform2),
+      new Spike(1050, -235, 50, 15, images.spike)
     ],
 
     decorations: [
@@ -29,7 +29,8 @@ function getDataLevel1() {
     ],
 
     checkpoints: [
-      new Checkpoint(635, -100, 30, 30, images.star)
+      new Checkpoint(635, -100, 30, 30, images.star),
+      new Checkpoint(1110, -250, 30, 30, images.star)
     ],
 
     player: new Player(250, 400, 30, 30, images.player, 0.3, 8, 3)
