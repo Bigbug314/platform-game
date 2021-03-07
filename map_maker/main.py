fae62bbdfc8baca23e0a312fca39bda627773d81
Bigbug314 <bigbug31415@gmail.com>
2021-03-07 12:44:12 +0100

    Update main.py

--- map_maker/main.py
+++ map_maker/main.py
@@ -4,14 +4,28 @@ from tkinter import font
 
 
 
+itemsName= [
+    "Platform1",
+    "Platform2",
+    "Moving platform1",
+    "Moving platform2",
+    "Bouncy platform",
+    "Spike",
+    "Checkpoint"
+]
+
+
 root = Tk()
 root.geometry("800x500")
 root.title("Map maker")
 
+canvas = Canvas(root, width=800, height=500, bg="snow2")
+canvas.pack()
+
 variable = StringVar(root)
-variable.set("one")
-dropDown = OptionMenu(root, variable, "one", "two", "three")
-dropDown.config(font=("Calibri",20))
-dropDown.pack()
+variable.set(itemsName[0])
+dropDown = OptionMenu(canvas, variable, *itemsName)
+dropDown.config(font=("Calibri",15))
+dropDown.place(x=600, y=0)
 
 root.mainloop()
