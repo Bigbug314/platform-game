fe8b48858de9b6551c53b9f121b62173f47a60c0
pokergamer <pokiv2@protonmail.com>
2021-03-07 15:33:11 +0300

    modified:   classes/GUI/button.js

--- classes/GUI/button.js
+++ classes/GUI/button.js
@@ -11,6 +11,9 @@ class Button extends GUI {
     }
 
     cooInRectangle(x, y) {
-
+      return (x < this.pos.x + this.size.x
+              && x > this.pos.x
+              && y < this.pos.y + this.size.h
+              && y > this.pos.y);
     }
 }
