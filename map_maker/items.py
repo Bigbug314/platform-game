cb881efa11a5c94b889564589f46bd95b7d9c92b
pokergamer <pokiv2@protonmail.com>
2021-03-07 14:13:42 +0300

    modified:   items.py

--- map_maker/items.py
+++ map_maker/items.py
@@ -1,42 +1,54 @@
 items_list = [
     {
+        "name": "background1",
+        "type": "background",
+        "image": "../images/background.png"
+    },
+    {
         "name": "player",
+        "type": "entity",
         "image": "../images/player.png",
         "default_width": 30,
         "default_height": 30
     },
     {
         "name": "grass_platform1",
+        "type": "platform",
         "image": "../images/platform1.png",
         "default_width": 100,
         "default_height": 20
     },
     {
         "name": "grass_platform2",
+        "type": "platform",
         "image": "../images/platform2.png",
         "default_width": 100,
         "default_height": 20
     },
     {
         "name": "checkpoint",
+        "type": "platform",
         "image": "../images/star.png",
         "default_width": 20,
         "default_height": 20
     },
     {
         "name": "spikes",
+        "type": "platform",
         "image": "../images/spike.png",
         "default_width": 100,
         "default_height": 20
     },
     {
         "name": "bouncy_platform",
+        "type": "platform",
         "image": "../images/slime.png",
         "default_width": 100,
         "default_height": 20
     },
     {
         "name": "brick_wall",
+        "type": "platform",
         "image": "../images/brickwall.png",
         "default_width": 20,
         "default_height": 100
